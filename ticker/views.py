8c43e508a27fbd079b36a8205d515ce33e61b31f
Shubham Gupta <AVI2147520@maricopa.edu>
2018-01-04 23:10:03 +0530

    view updated.

--- ticker/views.py
+++ ticker/views.py
@@ -7,10 +7,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
 def ticker(coin):
-    btc_price = get_btc_price()
-    #http = urllib3.PoolManager()
-    url = "https://b...content-available-to-author-only...x.com/Api/v2.0/pub/market/GetLatestTick"
+    btc_price = 0
+    # http = urllib3.PoolManager()
+    url = "https://bittrex.com/Api/v2.0/pub/market/GetLatestTick"
     querystring = {"marketName": coin, "tickInterval": "thirtyMin"}
 
     headers = {
@@ -33,11 +35,13 @@ def ticker(coin):
 
     return coin_price_in_dollars
 
-class GetBtcPrice(APIView):
-    renderer_classes = (JSONRenderer)
 
-    def get(self):
-        url = "https://b...content-available-to-author-only...x.com/Api/v2.0/pub/currencies/GetBTCPrice"
+class GetBtcPrice(APIView):
+
+    print('hiii')
+
+    def get(self, *args, **kwargs):
+        url = 'https://bittrex.com/Api/v2.0/pub/currencies/GetBTCPrice'
 
         headers = {
             'host': "bittrex.com",
@@ -45,7 +49,7 @@ class GetBtcPrice(APIView):
             'accept': "application/json, text/javascript, */*; q=0.01",
             'x-requested-with': "XMLHttpRequest",
             'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36",
-            'referer': "https://b...content-available-to-author-only...x.com/home/markets",
+            'referer': "https://bittrex.com/home/markets",
             'accept-encoding': "gzip, deflate, br",
             'accept-language': "en-US,en;q=0.8",
             'cookie': "__cfduid=dc8969bd3cf7ce991eb6db16dbb7864a61513575280; __RequestVerificationToken=rnZgQUn6IS4Qpi32YPKUBPaNmVEC57uOsd4rhXb7rAnaRkiiUvNWwxiq8N5VpjILIhG5SJlpjWszvoGRUM1TT-lLXHU1; cf_clearance=0413c3284b2afd4d1ba8b2d354ffd05fa9692dc3-1514964151-10800",
@@ -57,5 +61,3 @@ class GetBtcPrice(APIView):
         time_price = json.loads(response.text)['result']['time']['updated']
         print(time_price)
         return Response({'btc': json.loads(response.text)['result']['bpi']['USD']['rate_float']})
-
-
